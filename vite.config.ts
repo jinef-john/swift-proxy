9ef2a9273bb80f5a32fb1dbd6f0abb1148f85ce7
Jinef <jinefjohn@gmail.com>
2025-08-21 07:38:40 +0300

    feat: update deployment configurations and adjust base path for production

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,11 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/swift-proxy/",
+  base:
+    process.env.NODE_ENV === "production" &&
+    process.env.DEPLOY_TARGET === "github"
+      ? "/swift-proxy/"
+      : "/",
   plugins: [
     compression(),
     react(),
